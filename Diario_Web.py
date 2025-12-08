7e34109fee8ae9800e5381c2ba137dcb732063f1
professormarcoscarneirofaetec-sketch <professormarcoscarneirofaetec@gmail.com>
2025-12-08 05:04:37 -0300

    Update Diario_Web.py

--- Diario_Web.py
+++ Diario_Web.py
@@ -270,9 +270,9 @@ def main():
     # Tentamos ler as credenciais configuradas no arquivo secrets.toml.
     try:
         # O Python LÊ o valor de "app_password" (que é "20710350Mar#")
-        SENHA_CORRETA = st.secrets["app_password"] 
+        SENHA_CORRETA = st.secrets["20710350Mar#"] 
         # O Python LÊ o valor de "app_user" (que é "marcos")
-        usuario_correto = st.secrets["app_user"]
+        usuario_correto = st.secrets["marcos"]
     except KeyError:
         # Se os segredos não existirem no Streamlit Cloud, o login falha.
         SENHA_CORRETA = ""
